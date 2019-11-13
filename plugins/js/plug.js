24d2a836fa0e296e56b7a0dd90b6ce828cb42f31
Rana ElRefaey <57539666+Rana-ELRefaey@users.noreply.github.com>
2019-11-14 01:05:26 +0200

    Resturant Tamplate

---
+++ plugins/js/plug.js
@@ -0,0 +1,11 @@
+ $(document).ready(function () {
+ 	$(".div-img").hover(function () {
+ 		$(".div-img").css({
+  '-webkit-transform' : 'scale('1.2')',
+  '-moz-transform'    : 'scale('1.2')',
+  '-ms-transform'     : 'scale('1.2')',
+  '-o-transform'      : 'scale('1.2')',
+  'transform'         : 'scale('1.2')'
+});
+ 	})
+ })
